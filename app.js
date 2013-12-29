e735468e738c42a305fa6f9b9e5259de21873da4
Vinay Vyas <vbvyas@gmail.com>
2013-12-29 01:00:38 -0800

    Update app.js to take id as a parameter.

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/test', function(req, res) {
   var jsonFile = require('./test.json');
   res.json(jsonFile);
 });
+
+app.get('/test/:id', function(req, res) {
+  var data = require('./test.json');
+  var id = req.params.id;
+  if (id < 1) res.json({});
+  else res.json(data[id - 1]);
+})
 
 app.listen(3000);
