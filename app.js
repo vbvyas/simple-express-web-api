5290e65e446c4fcf4bbe0e246beb5b280de60e45
Vinay Vyas <vbvyas@gmail.com>
2014-01-17 10:23:27 -0800

    Express query on get

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/test', function(req, res) {
+  var query = req.query; // Do something with query
   var jsonFile = require('./test.json');
   res.json(jsonFile);
 });
